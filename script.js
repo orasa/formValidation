c090e717bb03df956330cc83f0773695ef0cd95a
orasa oneal <orasaoneal@gmail.com>
2020-10-27 00:20:26 -1000

    email validation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 // function showError
 
@@ -16,6 +19,13 @@ function showSuccess(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
 }
+
+//  Check email is valid with regular expression
+function isValidEmail(email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
 // Ad event listennig upon submit of the form
 
 form.addEventListener('submit', function(e) {
@@ -25,5 +35,19 @@ form.addEventListener('submit', function(e) {
 		showError(username, 'Username is required');
 	} else {
 		showSuccess(username);
+	}
+
+	if (email.value === '') {
+		showError(email, 'Email is required');
+	} else if (!isValidEmail(email.value)) {
+		showError(email, 'Email is invalid');
+	} else {
+		showSuccess(email);
+	}
+
+	if (password.value === '') {
+		showError(password, 'Password is required');
+	} else {
+		showSuccess(password);
 	}
 });
